e088027edd0a8599c1b7d5fc0ae9d6844bd1efd3
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-03-17 13:03:24 -0400

    Fix build on ci

--- home-ng/pre_release.sh
+++ home-ng/pre_release.sh
@@ -4,14 +4,29 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-rm -rfv /tmp/hongkailiu.github.io/
-git clone https://github.com/hongkailiu/hongkailiu.github.io.git /tmp/hongkailiu.github.io
-cp -r ./docs/* /tmp/hongkailiu.github.io/
-git -C /tmp/hongkailiu.github.io/ checkout README.md
-git -C /tmp/hongkailiu.github.io/ checkout CNAME
-git -C /tmp/hongkailiu.github.io/ add .
-git -C /tmp/hongkailiu.github.io/ status
+
+
+
+folder_suffix=local
+
+if [[ $(echo "${CIRCLECI}" | awk '{print tolower($0)}') == "true" ]]; then
+  folder_suffix="${CIRCLE_BUILD_NUM}"
+fi
+
+if [[ $(echo "${TRAVIS}" | awk '{print tolower($0)}') == "true" ]]; then
+  folder_suffix="${TRAVIS_JOB_NUMBER}"
+fi
+
+readonly TARGET_FOLDER="/tmp/hongkailiu.github.io-${folder_suffix}/"
+
+rm -rfv "${TARGET_FOLDER}"
+git clone https://github.com/hongkailiu/hongkailiu.github.io.git "${TARGET_FOLDER}"
+cp -rv ./docs/* "${TARGET_FOLDER}"
+git -C "${TARGET_FOLDER}" checkout README.md
+git -C "${TARGET_FOLDER}" checkout CNAME
+git -C "${TARGET_FOLDER}" add .
+git -C "${TARGET_FOLDER}" status
 #$(eval COMMIT_MSG := "$(shell git log --oneline -1)")
 readonly COMMIT_MSG="$(git log --oneline -1)"
-git -C /tmp/hongkailiu.github.io/ commit -m "${COMMIT_MSG}"
-git -C /tmp/hongkailiu.github.io/ log -2
+git -C "${TARGET_FOLDER}" commit -m "${COMMIT_MSG}"
+git -C "${TARGET_FOLDER}" log -2
